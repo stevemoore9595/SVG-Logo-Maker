4ec891e049884bf12c972f72d463ce9b8f61214c
stevemoore9595 <stevemoore9595@gmail.com>
2023-03-02 23:04:13 -0700

    added text and text color to the Shape class, finished the init function, and added the svg

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Packages needed for this application
-import inquirer from 'inquirer';
-import { Triangle, Circle, Square } from './lib/shapes.js';
+// import inquirer from 'inquirer';
+const inquirer = require("inquirer");
+
+const {Shape, Triangle, Circle, Square } = require('./lib/shapes.js');
+// import { Triangle, Circle, Square } from './lib/shapes.js';
+// import fs from 'fs';
 const fs = require('fs');
 
 // An array of questions for user input
@@ -11,7 +15,7 @@ const questions = [{
 }, {
     type: 'input',
     message:'Enter a text color',
-    name:'Text-Color'
+    name:'TextColor'
 }, {
     type: 'list',
     message:'Choose a shape from the list',
@@ -20,12 +24,12 @@ const questions = [{
 }, {
     type: 'input',
     message:'Enter a shape color',
-    name:'Shape-color'
+    name:'ShapeColor'
 }];
 // Function to create the logo
 function writeToFile(filename, data) {
 
-    fs.writeFile(filename, {Triangle, Circle, Square}(data), error => {
+    fs.writeFile(filename, data, error => {
         if (error) {
            return console.log(error)
         } else {
@@ -34,23 +38,26 @@ function writeToFile(filename, data) {
     });
 };
 // Function to initialize the app
-inquirer.prompt(questions) 
-    .then((data) => {
-    if ('data.Triangle' === Shape) {
-        return Triangle
-    } else if ('data.Circle' === Shape) {
-        return Circle
-    } else {
-        return Square
-    } 
-});
+
 
 function init() {
-    return inquirer.prompt(questions)
+   return inquirer.prompt(questions) 
     .then((data) => {
-        writeToFile('logo.svg', data);
-        console.log(data);
-    }); 
+        console.log(data.ShapeColor)
+        let userShape 
+    if ('Triangle' === data.Shape) {
+        userShape = new Triangle
+    } else if ('Circle' === data.Shape) {
+        userShape = new Circle
+    } else {
+        userShape = new Square
+    } userShape.setColor(data.ShapeColor)
+    userShape.setTextColor(data.TextColor)
+    userShape.setText(data.Text)
+    console.log(userShape)
+    return writeToFile('logo.svg', userShape.renderTriangle());
+})
 };
 
 init()
+
